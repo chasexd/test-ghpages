43286445eb4dea4f45f5f47dbeaf855813baa09b
Ashish Jayan <ashishkjay@gmail.com>
2020-04-13 18:56:26 -0500

    update

--- next.config.js
+++ next.config.js
@@ -9,28 +9,28 @@ module.exports = {
     return {
       "/": { page: "/" },
       "/about": { page: "/about" },
-    }
+    };
   },
   //assetPrefix: '',
-  assetPrefix: !debug ? '/Next-gh-page-example/' : '',
+  assetPrefix: !debug ? "/test-ghpages/" : "",
   webpack: (config, { dev }) => {
     // Perform customizations to webpack config
     // console.log('webpack');
     // console.log(config.module.rules, dev);
-    config.module.rules = config.module.rules.map(rule => {
-      if(rule.loader === 'babel-loader') {
-        rule.options.cacheDirectory = false
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule.loader === "babel-loader") {
+        rule.options.cacheDirectory = false;
       }
-      return rule
-    })
+      return rule;
+    });
     // Important: return the modified config
-    return config
-  }/*,
+    return config;
+  } /*,
   webpackDevMiddleware: (config) => {
     // Perform customizations to webpack dev middleware config
     // console.log('webpackDevMiddleware');
     // console.log(config);
     // Important: return the modified config
     return config
-  }, */
-}
+  }, */,
+};
